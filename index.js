15bc14571af0b7677524b9fae58d4c4a64463c28
ryry <ryanpetrecca@gmail.com>
2021-01-02 21:34:44 -0800

    finished typing out questions to be asked through inquirer

--- index.js
+++ index.js
@@ -1,26 +1,67 @@
 const inquirer = require("inquirer");
-
-const generateReadme = require("./utils/generateMarkdown.js")
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 const promptUser = function () {
     inquirer.prompt([
         {
             type: "input",
-            name: "title",
             message: "What is the title of your project",
+            name: "title"
         },
         {
             type: "input",
-            name: "description",
-            message: "Write a brief description of your project: "
+            message: "Write a brief description of your project: ",
+            name: "description"
         },
         {
             type: "input",
-            name: "installation",
             message: "Describe the installation process if any: ",
+            name: "installation"
         },
         {
-            
+            type: "input",
+            message: "How do you use this app/project?",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ],
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "Who are the contributors of this project?",
+            name: "contributing",
+        },
+        {
+            type: "input",
+            message: "Is there a test included?",
+            name: "tests",
+        },
+        {
+            type: "input",
+            message: "Please enter your GitHub username: ", 
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Please enter your email: ",
+            name: "email"
         }
-    ])
-}
+
+    ]).then((data) => {
+        // generateMarkdown(data);
+    });
+};
+
+promptUser();
